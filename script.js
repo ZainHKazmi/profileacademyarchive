b99ab859b3d20743143b27fe991f56614feb285d
Zain Kazmi <zain.kazmi@mail.utoronto.ca>
2019-05-17 09:29:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Profile Academy. All rights reserved.</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='instagram.html'>Instagram</a></li>" +
+"<li><a href='youtube.html'>Youtube</a></li>" +
+"<li><a href='facebook.html'>Facebook</a></li>" +
+"<li><a href='twitter.html'>Twitter</a></li>" +
+"<li><a href='contact.html'>Contact Us</a></li>" +
+"</ul>";
